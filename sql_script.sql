b197373923939d59b603b3b7f0d41f9a7db68030
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-07-02 19:10:12 +0600

    some complex analysis domne

--- sql_script.sql
+++ sql_script.sql
@@ -124,3 +124,95 @@ inner join products as p on p.id = i.product_id
 group by 1, 2;
 
 
+
+
+
+-- Rank customers by total amount spent.
+select
+	c.full_name,
+	sum(o.total_amount) as amount_spend,
+	rank() over(order by sum(o.total_amount) desc) as customer_rank
+from customers as c
+inner join orders as o on c.id = o.customer_id
+group by 1;
+
+
+
+
+
+-- Show the top 3 best-selling products per store.
+with top_3_product as(
+select
+	s.store_name,
+	p.name,
+	sum(oi.quantity) as sold,
+	rank() over(partition by s.store_name order by sum(oi.quantity) desc ) as rank_
+from products as p
+inner join order_items as oi on p.id = oi.product_id
+inner join orders as o on oi.order_id = o.id
+inner join stores as s on s.id = o.store_id
+group by 1, 2
+)
+
+select *
+from top_3_product
+where rank_ <= 3;
+
+
+
+
+
+
+
+-- Calculate a running total of daily revenue.
+select
+	 date(created_at) as order_date,
+	 sum(price) as daily_revenue,
+	 sum(sum(price)) over(order by date(created_at)) as runnig_total_revenue
+from order_items
+group by 1
+order by 1;
+
+
+
+
+-- Compute a 7-day rolling average of total order amounts.
+with daily_order_totals as (
+select 
+	date(created_at) as order_date,
+	sum(price) as daily_revenue
+from order_items
+group by 1
+),
+rolling_avg as(
+select 
+	order_date,
+	daily_revenue,
+	round(avg(daily_revenue) over(order by order_date rows between 6 preceding and current row),2) as rolling_avg_7_day
+from daily_order_totals
+)
+select *
+from rolling_avg
+order by 1
+
+
+
+
+-- Show the time difference between each customer's consecutive orders.
+with time_diff as(
+select
+  c.full_name as full_name,
+  o.id as current_order_id,
+  date(o.created_at) as current_order_date,
+  lag(date(o.created_at)) over (partition by c.full_name order by o.created_at) as previous_order_date,
+  o.created_at - lag(o.created_at) over (partition by c.full_name order by o.created_at) as time_diff
+from orders as o
+inner join customers as c on c.id = o.customer_id
+order by c.full_name, o.created_at
+)
+select *
+from time_diff
+where time_diff is not null
+
+
+
